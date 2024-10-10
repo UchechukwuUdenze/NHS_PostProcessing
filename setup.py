c4349358e009a7b55b6381831a457c853d1155f5
Uchechukwu Udenze <Uchechukwu.Udenze@ec.gc.ca>
2024-10-10 16:46:30 -0600

    Modified the setup file to correct python_requires  in setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author='Uchechukwu Udenze',
     description='post processing library',
     long_description=long_description,
-    python_requires='3.11.9',
+    python_requires='3.11',
     install_requires=[
         'numpy',
         'pandas',
